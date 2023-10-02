52e02f23383c1b3069b1148107c89892d41c417a
nahnahAJ <nahnahahjuwah@gmail.com>
2023-10-02 17:02:42 +0000

    Include Data for the App

---
+++ data.rb
@@ -0,0 +1,60 @@
+# Properties for user https://alveohive.com/kt-admin/?o=users%2Findex.php&q=edit/f20b9e35970ad97af99d74e9ec0b8bd8/1697
+# Name: Nana Kelvin
+property1 = Property.create(
+  title: "Exquisite Four (4) Bedroom Full House For Sale", 
+  location: "Ashongman Estates, Ghana	", 
+  property_status: "For Sale",
+  images: "",
+  property_overview: "*4 bedroom full house For Sale @ Ashongman-Estates*
+*Details:
+-2 washrooms
+-Hall & Dining
+-Parking space
+-Fixed Acs & Fans
+-Fitted kitchen
+-Security wires
+-50ft by 80ft size",
+number_of_bedrooms: 4, 
+number_of_bathrooms: 2, 
+ratings: "", 
+amenities: "Fully fitted kitchen, AC, Security service , Waste disposal, Washroom, Parking space,  hall and dining", 
+furnishing: "Unfurnished", 
+size: "50ft by 80ft" 
+price: 700000, 
+date_listed: "2023-06-16 08:15:00")
+
+# Name: Ohemaa Dufie
+property2 = Property.create(
+  title: "Exquisite 4 Bedroom House For Sale",
+  location: "Spintex, Accra, Ghana",
+  property_status: "For Sale",
+  images: "",
+  property_overview: "Introducing this new 4-bedroom development coming up at Spintex, near HFC Estates. Boasts of a spacious living área, a maid's quarters, inner and outer kitchen, in-built wardrobes, fitted Kitchen, etc, all on 60ft × 90ft registered parcel. Selling for $350,000 (negotiable) NB: No agency fees involved",
+  number_of_bedrooms: 4,
+  number_of_bathrooms: 5,
+  ratings: "",
+  amenities: "",
+  furnishing: "",
+  size: "",
+  price: 3780000,
+  date_listed: "2023-02-27 07:38:00"
+  )
+
+  property3 = Property.create(
+    title: "Stunning 4 Bedroom House For Sale",
+    location: "Lashibi, Ghana",
+    property_status: "For Sale",
+    images: "",
+    property_overview: "Welcome to this stunning 4 bedroom house located in the peaceful community of Lashibi. This beautiful home features spacious rooms, natural light, and a comfortable living space. The well-designed kitchen comes equipped with modern appliances and ample storage. The bedrooms are cozy, perfect for rest and relaxation. Listing Price- $260,000 negotiable.",
+    number_of_bedrooms: 4,
+    number_of_bathrooms: 5,
+    ratings: "",
+    amenities: "",
+    furnishing: "",
+    size: "",
+    price: 2800000,
+    date_listed: "2023-02-09 02:09:00" 
+    )
+
+    
+
