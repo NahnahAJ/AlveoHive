9f43c56967f7bb2b5ceb9567ddc40cae8ca6499f
nahnahAJ <nahnahahjuwah@gmail.com>
2023-10-04 09:14:05 +0000

    Modify the user Model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-    has_secure_password
-  
     has_one :user_detail 
     has_many :properties
     has_many :articles
@@ -8,6 +6,5 @@ class User < ApplicationRecord
   
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
-    validates :password, presence: true, length: { minimum: 6 }
   end
   
