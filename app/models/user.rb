1e958574d16942ae6610cacca9ca81a609162610
nahnahAJ <nahnahahjuwah@gmail.com>
2023-10-06 15:17:28 +0000

    Add devise to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
-    has_one :user_detail 
-    has_many :properties
-    has_many :articles
-    has_many :enquiries
-  
-    validates :username, presence: true, uniqueness: true
-    validates :email, presence: true, uniqueness: true
-  end
-  
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         has_one :user_detail 
+         has_many :properties
+         has_many :articles
+         has_many :enquiries
+       
+         validates :username, presence: true, uniqueness: true
+         validates :email, presence: true, uniqueness: true
+          validates :password, presence: true, length: { minimum: 6 }
+end
