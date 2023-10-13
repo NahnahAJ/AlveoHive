9beacf2ecb10d0229353ca4a894962b1ffeaac2b
nahnahAJ <nahnahahjuwah@gmail.com>
2023-10-13 09:32:27 +0000

    Include jwt authentication to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,15 @@ class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :validatable,
+  :jwt_authenticatable, jwt_revocation_strategy: self 
 
          has_one :user_detail 
          has_many :properties
          has_many :articles
          has_many :enquiries
        
-         validates :username, presence: true, uniqueness: true
+         validates :username, uniqueness: true
          validates :email, presence: true, uniqueness: true
           validates :password, presence: true, length: { minimum: 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,6 @@ Rails.application.routes.draw do
       registrations: 'users/registrations'
     }
 
-# Routes to view the profile_picture
-
 namespace :api do 
   namespace :v1 do 
     resources :users, only: [:index, :create, :show, :destroy]
