319c2ace7faa4f318c238ec0e5d26b5969b7dc65
nahnahAJ <nahnahahjuwah@gmail.com>
2023-11-01 12:24:30 +0000

    Make some changes to the ability.rb file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,22 +3,30 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the user here. For example:
-
     user ||= User.new # guest user (not logged in)
-    
-    can :read, Property
-    can :read, Article
 
     if user.admin? || user.persisted?
       can :manage, :all
     else
-      can :manage, User, id: user.id
-      can :manage, UserDetail, user_id: user.id
-      can :manage, Social, user_id: user.id
-      can :manage, Property, user_id: user.id
-      can :manage, Article, user_id: user.id
-      can :manage, Enquiry, user_id: user.id
+      can :read, :all
+      can :create, Property
+      can :create, Article
+      can :create, UserDetail
+      can :create, Category
+      can :create, Social
+      can :create, Enquiry
+      can :create, Amenity
+      can :create, Currency
+
+
+      can [:update, :destroy], Property, user_id: user.id
+      can [:update, :destroy], Article, user_id: user.id
+      can [:update, :destroy], UserDetail, user_id: user.id
+      can [:update, :destroy], Category
+      can [:update, :destroy], Social, user_id: user.id
+      can [:update, :destroy], Enquiry, user_id: user.id
+      can [:update, :destroy], Amenity, user_id: user.id
+      can [:update, :destroy], Currency, user_id: user.id
     end
   end
 end
