f7077e6468d2ab231db922181534d76678399760
nahnahAJ <nahnahahjuwah@gmail.com>
2023-11-01 12:23:45 +0000

    Add exception message from cancancan to Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 # app/controllers/application_controller.rb
 
 class ApplicationController < ActionController::API
-
+rescue_from CanCan::AccessDenied do |exception|
+  render json: { message: exception.message }, status: :forbidden
+end
 end
