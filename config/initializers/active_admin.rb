8337c39a038e0a98f4e65489866cc1807b48051d
Michael Osei <oseim2154@gmail.com>
2024-06-09 20:24:12 +0000

    Update activeadmin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,6 +5,13 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "Alveo Hive Admin"
+  
+  # Set asset prefix
+  config.asset_host = '/data'
+
+  # Set route prefix
+  config.default_namespace = 'data/admin'
+
   # config.paginate = :will_paginate
 
   # Set the link url for the title. For example, to take
