6398d6cb534227b7fbdf701b18df8d0802948ee4
Michael Osei <oseim2154@gmail.com>
2024-06-22 14:39:10 +0000

    Updated admin user model

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,14 @@
 class AdminUser < ApplicationRecord
+       validate :validate_max_admin_users, on: :create
        devise :database_authenticatable, :registerable,
        :recoverable, :rememberable, :validatable
+
+       private
+
+       def validate_max_admin_users
+         if AdminUser.count >= 1
+           errors.add(:base, "Maximum number of admin users reached. Only 1 admin is allowed.")
+         end
+       end
+
 end
