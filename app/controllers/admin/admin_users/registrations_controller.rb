d627e20cb17abac72eb98aab7a2a9c8a4ae05d15
Michael Osei <oseim2154@gmail.com>
2024-06-22 14:34:15 +0000

    Deleted custom active admin registration controller

--- app/controllers/admin/admin_users/registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-class AdminUsers::RegistrationsController < Devise::RegistrationsController
-    before_action :redirect_to_root, only: [:new, :create]
-  
-    private
-  
-    def redirect_to_root
-      redirect_to root_path, alert: "Sign up is not allowed."
-    end
-end
