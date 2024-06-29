e843803c310dc865d73a7775f09d9827699941e8
Michael Osei <oseim2154@gmail.com>
2024-06-29 14:28:11 +0000

    Test

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -185,20 +185,15 @@ module Api
         # GET /api/v1/properties/search
         def search
           # Join properties with user_details to access subscription status
-          @properties = Property.joins(user: :user_detail)
-          .where(is_property_live: true)
-          .filter_by_params(search_params)
-          .order(
-            Arel.sql("CASE 
-                       WHEN user_details.subscription = 'subscribed' AND 
-                            user_details.last_subscription_date >= '#{1.year.ago.to_s(:db)}' 
-                       THEN 0 
-                       ELSE 1 
-                     END, properties.created_at DESC")
-          )
-          .page(params[:page])
-          .per(params[:per_page] || 30)
-          .left_outer_joins(:user_detail)
+          # @properties = Property.joins(user: :user_detail)
+          @properties = Property.where(is_property_live: true)
+                                .filter_by_params(search_params)
+                                .order(created_at: :desc)
+                                # .order(
+                                #   Arel.sql("CASE WHEN user_details.subscription = 'subscribed' AND user_details.last_subscription_date >= '#{1.year.ago.to_s(:db)}' THEN 0 ELSE 1 END, properties.created_at DESC")
+                                # )
+                                .page(params[:page])
+                                .per(params[:per_page] || 30)
 
           if @properties.empty?
             # if no exact match is found, provide similar items as suggestions
