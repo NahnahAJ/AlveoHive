0526cf8e048086372832fdf752f731079903a4c3
Michael Osei <oseim2154@gmail.com>
2024-06-29 11:55:31 +0000

    Update properties_controller.rb

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -191,19 +191,12 @@ module Api
                               # .order(created_at: :desc)
                               .page(params[:page])
                               .per(params[:per_page] || 30)
-                              # .order(Arel.sql("CASE 
-                              # WHEN user_details.subscription = 'subscribed' 
-                              #  AND user_details.last_subscription_date >= '#{1.year.ago.to_s(:db)}' 
-                              # THEN 0 
-                              # ELSE 1 
-                              # END, properties.created_at DESC"))
-
-        @properties = Property.joins(user: :user_detail)
-        .where(is_property_live: true)
-        .filter_by_params(search_params)
-        .order(Arel.sql("CASE WHEN user_details.subscription = 'subscribed' AND user_details.last_subscription_date >= '#{1.year.ago.to_s(:db)}' THEN 0 ELSE 1 END, properties.created_at DESC"))
-        .page(params[:page])
-        .per(params[:per_page] || 30)
+                              .order(Arel.sql("CASE 
+                              WHEN user_details.subscription = 'subscribed' 
+                               AND user_details.last_subscription_date >= '#{1.year.ago.to_s(:db)}' 
+                              THEN 0 
+                              ELSE 1 
+                              END, properties.created_at DESC"))
 
         if @properties.empty?
           # if no exact match is found, provide similar items as suggestions
