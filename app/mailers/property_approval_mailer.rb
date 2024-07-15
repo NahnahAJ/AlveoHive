02a4e297565440049ee5a376e58bf7cebc490115
Michael Osei <oseim2154@gmail.com>
2024-07-15 21:09:16 +0000

    Updated email templates

--- app/mailers/property_approval_mailer.rb
+++ app/mailers/property_approval_mailer.rb
@@ -3,8 +3,11 @@ class PropertyApprovalMailer < ApplicationMailer
     @property = property
     user = User.find_by(id: property.user_id)
     if user && property.is_property_live
-      # Send mail to only the user when property is approved
-      mail(to: user.email, subject: 'Property Approved on AlveoHive')
+      attachments.inline['logo.png'] = File.read(Rails.root.join('app/assets/images/alveohive-logo-full-color2.png'))
+      mail(to: user.email, subject: 'Property Approved on AlveoHive') do |format|
+        format.html
+        format.text
+      end
     end
   end
 
@@ -12,9 +15,11 @@ class PropertyApprovalMailer < ApplicationMailer
     @property = property
     user = User.find_by(id: property.user_id)
     if user
-      # Send mail to both the user and AlveoHive support when property is submitted
-      #recipients = [property.user.email.to_s, 'support@alveohive.com']
-      mail(to: user.email, subject: 'Property Submitted for Review on AlveoHive')
+      attachments.inline['logo.png'] = File.read(Rails.root.join('app/assets/images/alveohive-logo-full-color2.png'))
+      mail(to: user.email, subject: 'Property Submitted for Review on AlveoHive') do |format|
+        format.html
+        format.text
+      end
     end
   end
 end
